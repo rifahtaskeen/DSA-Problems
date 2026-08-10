dfac46038770ed45c79dae17f4ef93fa9187b9ab
Rifah Taskeen <rifahtaskeen16@gmail.com>
2026-08-10 21:52:27 +0530

    Brute force approach
    Brute approach:
    Use two loops,keep a variable count , check condition if the previous element is 2 times greater than the current element.
    TC-O(n^2)
    SC_O(1)

---
+++ 03_Arrays/Hard/Reverse Pairs/Brute_approach.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+  int reversePairs(vector<int>& nums) {
+    int n=nums.size();
+    int count=0;
+    for(int i=0;i<n;i++){
+      for(int j=i+1;j<n;j++){
+        if(nums[i]>2*nums[j]) count++;
+      }
+    }
+    return count;
+  }
+};
